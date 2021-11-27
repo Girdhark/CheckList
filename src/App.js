e988ad63609bc498e7bd1873d3a74b1a9402e8cb
GirdharK <girdharkataria02@gmail.com>
2021-11-27 21:54:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CheckList from './components/CheckList';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <CheckList />
+    </div>
+  );
+}
+
+export default App;
